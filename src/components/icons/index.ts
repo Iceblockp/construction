9c852b98f152066585ba4884dbb9a43156444f8a
Iceblockp <phonyo126@gmail.com>
2024-06-03 13:32:48 +0630

    icon add

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as ServiceOne } from "./ServiceOne";
+export { default as ServiceTwo } from "./ServiceTwo";
+export { default as ServiceThree } from "./ServiceThree";
+export { default as ServiceFour } from "./ServiceFour";
+export { default as ServiceFive } from "./ServiceFive";
+export { default as ServiceSix } from "./ServiceSix";
--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../layout/Container'
+import { ServiceOne } from '../icons'
 
 type Props = {}
 
@@ -9,12 +10,43 @@ const Services = (props: Props) => {
             <Container>
                 <div className='  py-[44px] text-center font-bold text-[36px] text-neutral-800 '>Services</div>
                 <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[50px] px-[25px] md:px-0  '>
-                    <div className=' w-full md:w-[271px] mx-auto bg-white h-[181px] border border-gray-400 '>hello</div>
-                    <div className=' w-full md:w-[271px] mx-auto bg-[#2947A9] h-[181px] border border-gray-400 '>hello</div>
-                    <div className=' w-full md:w-[271px] mx-auto bg-white h-[181px] border border-gray-400 '>hello</div>
-                    <div className=' w-full md:w-[271px] mx-auto bg-[#2947A9] h-[181px] border border-gray-400 '>hello</div>
-                    <div className=' w-full md:w-[271px] mx-auto bg-white h-[181px] border border-gray-400 '>hello</div>
-                    <div className=' w-full md:w-[271px] mx-auto bg-[#2947A9] h-[181px] border border-gray-400 '>hello</div>
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-white h-[132.44px] border border-gray-400 flex flex-col items-center '>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-neutral-100 flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-[#2947A9] '>Construction</div>
+                    </div>
+
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-[#2947A9] h-[132.44px] border border-gray-400 flex flex-col items-center '>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-white flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-white '>Renovation</div>
+                    </div>
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-white h-[132.44px] border border-gray-400 flex flex-col items-center'>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-neutral-100 flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-[#2947A9] '>Construction</div>
+                    </div>
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-[#2947A9] h-[132.44px] border border-gray-400 flex flex-col items-center'>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-white flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-white '>Repair Services</div>
+                    </div>
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-white h-[132.44px] border border-gray-400 flex flex-col items-center'>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-neutral-100 flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-[#2947A9] '>Architecture</div>
+                    </div>
+                    <div className=' w-full md:w-[198.3px] mx-auto bg-[#2947A9] h-[132.44px] border border-gray-400 flex flex-col items-center'>
+                        <div className=' size-[44px] my-[20px] border-b-2 border-white flex justify-center items-center '>
+                            <ServiceOne />
+                        </div>
+                        <div className=' font-semibold text-[14.63px] text-white '>Electric</div>
+                    </div>
                 </div>
             </Container>
         </div>
